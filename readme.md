ad9e27988b0a22250494cd2ed43fc64b09ed966a
Peter <deadforstree@gmail.com>
2023-07-26 13:10:44 +0300

    functional

--- readme.md
+++ readme.md
@@ -1,2 +1,9 @@
-Ссылка на макет в Figma: https://disk.yandex.ru/d/0zgbWr5IcJ1aOA
-zzz
+# movies-explorer-frontend
+
+[Макет Дипломной работы](https://disk.yandex.ru/d/0zgbWr5IcJ1aOA)
+
+ссылка на пул-реквест: 
+#
+
+# адрес готового проекта:
+https://deadforstree.nomoreparties.sbs
--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import './PageNotFound.css';
 
 function PageNotFound() {
+    const history = useNavigate();
+
     return (
         <section className="not-found">
             <h2 className="not-found__title">404</h2>
             <p className="not-found__text">Страница не найдена</p>
-            <Link to="/" className="not-found__link">Назад</Link>
+            <button className="not-found__link" onClick={() => history.goBack()}>Назад</button>
         </section>
     );
 }
